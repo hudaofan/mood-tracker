e2c6dffb42e6c7e0f6f26249f2b2af72fd36c951
BrainShark <1102052725@qq.com>
2025-08-26 19:45:31 +0800

    ci :ferris_wheel: logo

---
+++ public/sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'mood-tracker-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/favicon.svg',
+  '/logo.svg',
+  '/manifest.json'
+];
+
+// 安装事件
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 获取事件
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // 如果缓存中有响应，则返回缓存的版本
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// 激活事件
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
